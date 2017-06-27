a4df8464be5f8ce2703310cb7fda584fac74c813
LePhuc <lecaobaophuc@gmail.com>
2017-06-28 00:05:14 +0700

    Update DKCarouselView.m

--- DKCarouselView/DKCarouselView.m
+++ DKCarouselView/DKCarouselView.m
@@ -401,7 +401,7 @@ typedef void(^DKCarouselViewTapBlock)();
     if (scrollView.isDragging) { 
         self.autoPagingTimer.fireDate = [NSDate dateWithTimeIntervalSinceNow:self.autoPagingTimer.timeInterval];
         
-        if (self.carouselItemViews.count == 2) {
+       /* if (self.carouselItemViews.count == 2) {
             if (scrollView.contentOffset.x < kScrollViewFrameWidth) {
                 UIView *previousView = self.carouselItemViews[GetPreviousIndex()];
                 if (!CGRectEqualToRect(CGRectMake(0, 0, kScrollViewFrameWidth, kScrollViewFrameHeight), previousView.frame)) {
@@ -413,7 +413,7 @@ typedef void(^DKCarouselViewTapBlock)();
                     [self insertNextPage];
                 }
             }
-        }
+        }*/
         
         if (self.didScrollBlock != nil) {
             self.didScrollBlock(self, scrollView.contentOffset.x);
